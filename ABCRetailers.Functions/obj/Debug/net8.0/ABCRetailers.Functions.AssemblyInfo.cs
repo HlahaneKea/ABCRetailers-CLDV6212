13d9649f2fc0d43b6793ea337db62047552b725d
HlahaneKea <st10445678@imconnect.edu.za>
2025-11-15 03:25:59 +0200

    Finalize POE Part 3

--- ABCRetailers.Functions/obj/Debug/net8.0/ABCRetailers.Functions.AssemblyInfo.cs
+++ ABCRetailers.Functions/obj/Debug/net8.0/ABCRetailers.Functions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCRetailers.Functions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f627ceb4ea458178407b45a437f0dc5111711102")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCRetailers.Functions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCRetailers.Functions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
